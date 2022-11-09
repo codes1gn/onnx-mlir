4c02e20417975f77120223722464fc017ada6fc1
Albert Shi <codefisheng@gmail.com>
2022-11-10 07:28:35 +0800

    impl packup entire region into pblock

--- src/Transform/RaptorAutoParallel.cpp
+++ src/Transform/RaptorAutoParallel.cpp
@@ -12,11 +12,14 @@
 // dialect operations.
 //
 //===----------------------------------------------------------------------===//
+#include <cstdlib>
+#include <iostream>
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "llvm/ADT/DenseMap.h"
 
 #include "src/Dialect/Crt/CrtDialect.hpp"
 #include "src/Dialect/Crt/CrtOps.hpp"
@@ -27,6 +30,94 @@ using namespace mlir;
 using namespace onnx_mlir;
 
 namespace {
+
+// method that pack the entire region of funcOp into a pblock region
+LogicalResult packUpEntireRegionWithPBlock(func::FuncOp funcOp, MLIRContext *context) {
+  std::cout << "yes" << std::endl;
+  mlir::Region& region = funcOp.getBody();
+
+  SmallVector<Operation *, 16> worklist;
+  int count = 0;
+
+  region.front().walk([&](Operation *op) {
+    worklist.push_back(op);
+  });
+
+  crt::PhantomBlockOp pblock_op;
+  crt::YieldOp pterminator;
+  mlir::ModuleOp moduleOp = funcOp->getParentOfType<ModuleOp>();
+
+  mlir::Value pterminator_operand;
+
+  std::reverse(worklist.begin(), worklist.end());
+  for (auto op: worklist) {
+    // auto _moduleOp = llvm::cast<ModuleOp>();
+    // handle terminator, add pblock and link pblocks two side to head and tail
+    if (llvm::isa<func::ReturnOp>(op)) {
+      std::cout << "return " << count++ << std::endl;
+      op->dump();
+      OpBuilder builder(op);
+      pblock_op = builder.create<crt::PhantomBlockOp>(op->getLoc(), op->getOperand(0).getType(), op->getOperand(0));
+
+      // let arg0 -> pblock
+      pblock_op.getOperation()->replaceUsesOfWith(pblock_op.getOperand(), funcOp.getArgument(0));
+      mlir::Block& block = pblock_op.getBody().emplaceBlock();
+
+      // insert yield into pblock, let return operand -> yield operand
+      pterminator = builder.create<crt::YieldOp>(op->getLoc(), op->getOperand(0));
+      pterminator.getOperation()->remove();
+      block.push_back(pterminator);
+      // pterminator.getOperation()->replaceUsesOfWith(pterminator.getOperand(), op->getOperand(0));
+
+      // TODO assume return single value
+      // let pblock -> return
+      op->replaceUsesOfWith(op->getOperand(0), pblock_op.getResult());
+    } else {
+      op->remove();
+      pblock_op.getBody().front().push_front(op);
+      // current all handle together, but should classify by result num
+    }
+  }
+  // pterminator.getOperation()->replaceUsesOfWith(pterminator.getOperand(), op->getResult(0));
+
+  // funcOp.walk([&](crt::IdentityOp op) {
+  //     OpBuilder builder(op);
+  //     // add new inserted op, a region op
+  //     auto pblock_op = builder.create<crt::PhantomBlockOp>(op.getLoc(), op.getType(), op.getOperand());
+  //     // clone former op into new one;
+  //     // can we use .clone()
+  //     auto newidop = builder.create<crt::IdentityOp>(op.getLoc(), op.getType(), op.getOperand());
+  //
+  //     // unlink new-clone and move into region op first block first position
+  //     newidop.getOperation()->remove();
+  //     Block& block = pblock_op.getBody().emplaceBlock();
+  //     block.push_back(newidop);
+  //     newidop.getOperation()->replaceUsesOfWith(newidop.getOperand(), pblock_op.getOperand());
+  //
+  //     // set terminator for pblock_op
+  //     auto _terminator = builder.create<crt::YieldOp>(op.getLoc(), newidop.getResult());
+  //     _terminator.getOperation()->remove();
+  //     block.push_back(_terminator);
+  //
+  //
+  //     // unlink op's users and link to region op results
+  //     auto nextOps = op.getResult().getUsers();
+  //     for (auto nextOp: nextOps) {
+  //       // cannot link, we may can use terminator to do it
+  //       // ref 1: search OpBuilder builder(op) 
+  //       // ref 2: link to onnx-mlir/docs/lowering.
+  //       // nextOp->replaceUsesOfWith(nextOp->getOperand(0), newidop.getResult());
+  //       nextOp->replaceUsesOfWith(nextOp->getOperand(0), pblock_op.getResult());
+  //     }
+  //
+  //     // drop legacy op links and erase it.
+  //     op.getOperation()->dropAllReferences();
+  //     op.getOperation()->dropAllUses();
+  //     op.getOperation()->dropAllDefinedValueUses();
+  //     op.erase();
+  // });
+  return success();
+}
 
 // method that pack the given op into a pblock
 template<typename OpType> 
@@ -43,6 +134,7 @@ LogicalResult packUpSpecOpWithPBlock(func::FuncOp funcOp, MLIRContext *context) 
       newidop.getOperation()->remove();
       Block& block = pblock_op.getBody().emplaceBlock();
       block.push_back(newidop);
+      newidop.getOperation()->replaceUsesOfWith(newidop.getOperand(), pblock_op.getOperand());
 
       // set terminator for pblock_op
       auto _terminator = builder.create<crt::YieldOp>(op.getLoc(), newidop.getResult());
@@ -83,7 +175,19 @@ struct RaptorAutoParallelPass
     func::FuncOp funcOp = getOperation();
     MLIRContext *context = &getContext();
 
-    packUpSpecOpWithPBlock<crt::IdentityOp>(funcOp, context);
+    if (const char* env_p = std::getenv("RAP")) {
+      if (std::strcmp(env_p, "test-pack-one") == 0) {
+        packUpSpecOpWithPBlock<crt::IdentityOp>(funcOp, context);
+      } else if (std::strcmp(env_p, "test-pack-all") == 0) {
+        packUpEntireRegionWithPBlock(funcOp, context);
+      }
+      else {
+        std::cout << env_p << "\n";
+        std::cout << "no matching mode for RAP envs";
+      }
+    } else {
+      std::cout << "please set RAP environment";
+    }
   }
 };
 
--- test/mlir/crt/unary/entire_region_to_pblock.mlir
+++ test/mlir/crt/unary/entire_region_to_pblock.mlir
@@ -1,14 +1,14 @@
-// RUN: RAP=test-pack-one onnx-mlir-opt %s --raptor-auto-parallel -split-input-file | FileCheck %s
+// RUN: RAP=test-pack-all onnx-mlir-opt %s --raptor-auto-parallel -split-input-file | FileCheck %s
 
 // CHECK-LABEL: func.func @crt_identity_test_shapedtensor_ret
 func.func @crt_identity_test_shapedtensor_ret(%arg0: tensor<17x16xf32>) -> tensor<17x16xf32>{
-  // CHECK: crt.exp %arg0 : (tensor<17x16xf32>) -> tensor<17x16xf32>
+  // CHECK: %3 = crt.pblock %arg0 : (tensor<17x16xf32>) -> tensor<17x16xf32> {
   %0 = crt.exp %arg0: (tensor<17x16xf32>) -> tensor<17x16xf32>
   %1 = crt.identity %0: (tensor<17x16xf32>) -> tensor<17x16xf32>
-  // CHECK: %1 = crt.pblock %0 : (tensor<17x16xf32>) -> tensor<17x16xf32> {
-  // CHECK: }
+  // CHECK: crt.identity %0 : (tensor<17x16xf32>) -> tensor<17x16xf32>
   %2 = crt.exp %1: (tensor<17x16xf32>) -> tensor<17x16xf32>
   %3 = crt.relu %2: (tensor<17x16xf32>) -> tensor<17x16xf32>
+  // CHECK: }
   return %3 : tensor<17x16xf32>
 }
 
